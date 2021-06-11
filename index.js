dae9efc9ec544e7439709cb2c2bc5fd8028bbf73
worldwidepark <83998627+worldwidepark@users.noreply.github.com>
2021-06-12 01:31:03 +0900

    learn about addeventlistener

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-const title = document.getElementById('title');
+const title = document.getElementById('h1');
 title.innerHTML = 'HI!';
+
+
+function handleTitleClick(){
+    title.style.color = 'blue';
+}
+
+function handleMouseEnter(){
+  title.innerHTML="stop"
+}
+
+function handleMouseLeasve(){
+    title.innerHTML="h1"
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato"; 
+}
+// there are three head, title, body can come back of document.
+
+function handleCopy(){
+    alert("copier")
+}
+function handleWindowOffline(){
+    alert("SOS no WIFI");
+}
+
+function handleWindowOnline(){
+    alert("GOOD");
+}
+
+
+title.addEventListener('click',handleTitleClick);
+// title.onclick();
+
+title.addEventListener('mouseenter',handleMouseEnter);
+title.addEventListener('mouseleave',handleMouseLeasve);
+
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener('copy',handleCopy);
+window.addEventListener("offline",handleWindowOffline);
+window.addEventListener("online",handleWindowOnline);
