74adc66db4305ae0f83e9debc2d4bc24ccee637d
worldwidepark <83998627+worldwidepark@users.noreply.github.com>
2021-06-13 01:33:36 +0900

    todo list started
    i should go now, cuz baby is crying !

---
+++ app.js
@@ -0,0 +1,16 @@
+/*
+const loginForm = document.querySelector("#login-form"); //querySelector choose id class etc so, needed 
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+*/
+
+ const loginInput = document.querySelector("#longin-form input");
+ const loginButton = document.querySelector("#longin-form button");
+
+
+function onLoginBtnClick(){
+    console.log("!!");
+}
+  
+
+loginButton.addEventListener("click", onLoginBtnClick);
