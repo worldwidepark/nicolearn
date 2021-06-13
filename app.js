86bb18c69bda44fc11a86812df7de3f7c2945d8a
worldwidepark <83998627+worldwidepark@users.noreply.github.com>
2021-06-14 01:05:51 +0900

    Add files via upload
    learn about form, submit, preventDefalut !  when button is in input, press button makse it submit and refresh!

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-/*
+
 const loginForm = document.querySelector("#login-form"); //querySelector choose id class etc so, needed 
-const loginInput = loginForm.querySelector("input");
+/*const loginInput = loginForm.querySelector("input");
 const loginButton = loginForm.querySelector("button");
 */
-
+/*
  const loginInput = document.querySelector("#longin-form input");
  const loginButton = document.querySelector("#longin-form button");
+*/
 
-
-function onLoginBtnClick(){
-    console.log("!!");
+function onLoginSubmit(event){
+    event.preventDefault();
+    console.log(event);
 }
-  
+loginForm.addEventListener("submit",onLoginSubmit);
 
-loginButton.addEventListener("click", onLoginBtnClick);
+
+  /*function onLoginBtnClick(){}
+  const username = loginInput.value;
+    if (username === ''){
+      alert("please write your name");
+  } else if(username.length> 15){
+        alert('your name is too long.');
+  }
+}
+*/
+
+
+
+//loginButton.addEventListener("click", onLoginBtnClick);
